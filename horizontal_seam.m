a79f6f54a476c3b893b32bd2b711eedd38137045
aviolaine <dudegirlsarah@gmail.com>
2017-12-13 01:55:21 -0600

    added horizontal seam algorithm

---
+++ horizontal_seam.m
@@ -0,0 +1,53 @@
+function seam = horizontal_seam(im, E)
+    [rows, cols, ~] = size(im);
+    
+    % create 2d array of subproblems
+    % (lowest-cost vertical seam if the seam ends at pixel i,j)
+    M = zeros(size(E));
+    M(:,1) = E(:,1);
+    for j = 2:cols
+        for i = 1:rows
+            center = M(i, j-1);
+            if i == 1
+                up = inf;
+                down = M(i+1, j-1);
+            elseif i == rows
+                up = M(i-1, j-1);
+                down = inf;
+            else
+                up = M(i-1, j-1);
+                down = M(i+1, j-1);
+            end
+            
+            minVal = min(min(up, down), center);
+
+            M(i, j) = E(i, j) + minVal;
+        end
+    end
+
+    %find starting point (min energy in last col)
+    [~, idx] = min(M(2:rows-1, end));
+
+    %backtrack leftward to find best horizontal seam
+    seam = false(rows,cols);
+    seam(idx,cols) = true; 
+    for j = cols-1:-1:1
+        up = inf;
+        if idx ~= 1
+            up = M(idx-1, j);
+        end
+        
+        center = M(idx, j);
+        
+        down = inf;
+        if idx ~= rows
+            down = M(idx+1, j);
+        end
+
+        [~, i] = min(cat(3, up, center, down));
+        seam(idx+i-2,j) = true;
+        idx = idx+i-2;
+    end
+    
+    figure(), imagesc(M), colormap jet;
+end
